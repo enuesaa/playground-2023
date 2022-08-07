aa16292e128fb6bcda7aad725d4ffb1b04b10322
nsrciog <nsrciog@gmail.com>
2022-08-07 19:07:04 +0900

    feat: enable to capture html

--- src/components/common/Main.tsx
+++ src/components/common/Main.tsx
@@ -1,15 +1,41 @@
 import { ReactNode } from 'react'
 import styles from './Main.module.scss'
+import html2canvas from 'html2canvas'
+import {useRef} from 'react'
 
 type Props = {
   children: ReactNode
 }
 export default function Main({ children }: Props) {
+  const captureTarget = useRef<HTMLElement>(null)
+
+  const capture = () => {
+    const target = captureTarget.current
+    if (target === null) {
+      return
+    }
+    html2canvas(target).then((canvas) => {
+      const uri = canvas.toDataURL('img/png')
+      const downloadLink = document.createElement('a')
+
+      if (typeof downloadLink.download === 'string') {
+        downloadLink.href = uri;
+        downloadLink.download = 'sample.png'
+        document.body.appendChild(downloadLink);
+        downloadLink.click();
+        document.body.removeChild(downloadLink);
+      } else {
+        window.open(uri);
+      }
+    })
+  }
+
   return (
     <>
-      <section className={styles.main}>
+      <section className={styles.main} ref={captureTarget}>
         {children}
       </section>
+      <button onClick={capture}>capture</button>
     </>
   )
 }
