67fa1228af311e9e9719d11ed6d757d338f75cc4
enuesaa <nsrciog@gmail.com>
2022-09-18 16:00:53 +0900

    chore: add example code as comment

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,31 @@
 
 export const sampleFnc = () => 'a';
+
+// export type Definition = {
+//   name: string,
+//   meta: Record<string, any>,
+//   main: () => void,
+// }
+
+// export class FuroshikiMatcher {
+//   definitions: Array<Definition>;
+
+//   constructor () {
+//     this.definitions = [];
+//   }
+
+//   addDefinition(arg: Definition) {}
+
+//   /**
+//    * return one definition
+//    */
+//   match() {
+//     return {matched, error}
+//   }
+// }
+
+// const matcher = new FuroshikiMatcher();
+// matcher.addDefinition({name: '', meta: {}, main: () => {}});
+// matcher.addDefinition({name: '', meta: {}, main: () => {}});
+
+// const {matched, error} = matcher.match()
