7fd9ad2875cead649a69a2e956b0beed577eb19b
enuesaa <nsrciog@gmail.com>
2022-09-27 12:31:11 +0900

    feat: tailwind のテストこーdより tailwindcssを構築できるであろうコードを追加した

--- bin/tailwindow.js
+++ bin/tailwindow.js
@@ -1,6 +1,26 @@
 #!/usr/bin/env node
 
-console.log('a')
+// from test code
+import postcss from 'postcss'
+import tailwind from 'tailwindcss'
+
+const tailwindPlugin = tailwind({
+  // content: [],
+  corePlugins: { preflight: false },
+})
+const postcssProcessor = postcss(tailwindPlugin)
+const input = String.raw`
+  @tailwind base;
+  @tailwind components;
+  @tailwind utilities;
+`
+
+const res = postcssProcessor.process(input)
+console.log(res)
+
+
+// import a from 'tailwindcss/src/corePlugins.js'
+// console.log(a.corePlugins)
 
 // load tailwind default config
 // parse process.argv
