3fc093ae7d3209e9b8c8e9864326e9bfb0d6da61
enuesaa <nsrciog@gmail.com>
2022-09-27 19:20:41 +0900

    feat: enable to print utilities of corePlugins

--- bin/tailwindow.js
+++ bin/tailwindow.js
@@ -1,27 +1,50 @@
 #!/usr/bin/env node
 
-// from test code
 import postcss from 'postcss'
 import tailwind from 'tailwindcss'
+import a from 'tailwindcss/src/corePlugins'
 
-const tailwindPlugin = tailwind({
-  // content: [],
-  corePlugins: { preflight: false },
-})
-const postcssProcessor = postcss(tailwindPlugin)
-const input = String.raw`
-  @tailwind base;
-  @tailwind components;
-  @tailwind utilities;
-`
+/**
+ * @see https://tailwindcss.com/docs/plugins
+ */
+let corePlugins = a.corePlugins
+let utilities = {}
+const addUtilities = (args) => {
+  utilities = Object.assign(utilities, args)
+}
+for (const key of Object.keys(corePlugins)) {
+  const fnc = corePlugins[key]
+  fnc({
+    addBase: () => {},
+    addUtilities: addUtilities,
+    theme: () => {},
+    addComponents: () => {},
+    matchUtilities: () => {},
+    addDefaults: () => {},
+    config: () => ({ future: 'all', }),
+  })
+}
 
-const res = postcssProcessor.process(input)
-console.log(res)
+console.log(utilities)
+
+// const tailwindPlugin = tailwind({
+//   // content: [],
+//   corePlugins: { preflight: false },
+// })
+// const postcssProcessor = postcss(tailwindPlugin)
+// const input = String.raw`
+//   @tailwind base;
+//   @tailwind components;
+//   @tailwind utilities;
+// `
+
+// const res = postcssProcessor.process(input)
+// console.log(res)
 
 
-// import a from 'tailwindcss/src/corePlugins.js'
-// console.log(a.corePlugins)
+// // import a from 'tailwindcss/src/corePlugins.js'
+// // console.log(a.corePlugins)
 
-// load tailwind default config
-// parse process.argv
-// out tailwind css
+// // load tailwind default config
+// // parse process.argv
+// // out tailwind css
