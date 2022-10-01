ab85bc1ac622ea801ccceb0181273ba710144649
enuesaa <nsrciog@gmail.com>
2022-10-02 03:22:18 +0900

    feat: `gh repo list` を内部的に実行できるようにした

--- cmd/list.go
+++ cmd/list.go
@@ -3,6 +3,7 @@ package cmd
 import (
 	"fmt"
 
+	"github.com/cli/go-gh"
 	"github.com/spf13/cobra"
 )
 
@@ -10,9 +11,20 @@ var listCmd = &cobra.Command{
 	Use: "list",
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("list command")
+		ghlist()
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(listCmd)
+}
+
+func ghlist() {
+	args := []string{"repo", "list"}
+	stdOut, _, err := gh.Exec(args...)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(stdOut.String())
 }
