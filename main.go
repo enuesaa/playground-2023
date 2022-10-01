94a6acfc86964c734ac2f7a12cbcdeefdba0fc3b
enuesaa <nsrciog@gmail.com>
2022-10-02 02:36:53 +0900

    feat: setup spf13/cobra

--- main.go
+++ main.go
@@ -1,9 +1,30 @@
 package main
 
 import (
-   "fmt"
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
 )
 
+var rootCmd = &cobra.Command{
+	Use: "go-last-commit",
+}
+var listCmd = &cobra.Command{
+	Use: "list",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("list command")
+	},
+}
+
 func main() {
-   fmt.Println("hello")
+	// disable default commands
+	rootCmd.CompletionOptions.DisableDefaultCmd = true
+	rootCmd.SetHelpCommand(&cobra.Command{Hidden: true})
+
+	rootCmd.AddCommand(listCmd)
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
 }
