216b0caf88e36ee331af84822bdfb9582b263210
enuesaa <nsrciog@gmail.com>
2022-10-02 02:58:10 +0900

    feat: ディレクトリ構造を変更

---
+++ cmd/root.go
@@ -0,0 +1,22 @@
+package cmd
+
+import (
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use: "go-last-commit",
+}
+
+func Execute() {
+	// disable default commands
+	rootCmd.CompletionOptions.DisableDefaultCmd = true
+	rootCmd.SetHelpCommand(&cobra.Command{Hidden: true})
+
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
--- main.go
+++ main.go
@@ -1,30 +1,9 @@
 package main
 
 import (
-	"fmt"
-	"os"
-
-	"github.com/spf13/cobra"
+	"gh-last-commit/cmd"
 )
 
-var rootCmd = &cobra.Command{
-	Use: "go-last-commit",
-}
-var listCmd = &cobra.Command{
-	Use: "list",
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("list command")
-	},
-}
-
 func main() {
-	// disable default commands
-	rootCmd.CompletionOptions.DisableDefaultCmd = true
-	rootCmd.SetHelpCommand(&cobra.Command{Hidden: true})
-
-	rootCmd.AddCommand(listCmd)
-	err := rootCmd.Execute()
-	if err != nil {
-		os.Exit(1)
-	}
+	cmd.Execute()
 }
