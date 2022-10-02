8bbb4107a878380a3c78e2b5fd0fa0ba3b415c38
enuesaa <nsrciog@gmail.com>
2022-10-03 00:26:57 +0900

    feat: change cli interface

--- cmd/root.go
+++ cmd/root.go
@@ -4,13 +4,24 @@ import (
 	"encoding/json"
 	"fmt"
 	"os"
+	"strings"
 
 	"github.com/TylerBrock/colorjson"
+	"github.com/cli/go-gh"
 	"github.com/spf13/cobra"
 )
 
 var rootCmd = &cobra.Command{
-	Use: "last-commit",
+	Use: "last-commit <repository>",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("view command")
+		if len(args) != 1 {
+			fmt.Println("need: repository")
+			os.Exit(1)
+		}
+		owner, name := parseRepositoryOwnerAndName(args[0])
+		ghView(owner, name)
+	},
 }
 
 func Execute() {
@@ -22,6 +33,63 @@ func Execute() {
 	if err != nil {
 		os.Exit(1)
 	}
+}
+
+func parseRepositoryOwnerAndName(str string) (string, string) {
+	result := strings.Split(str, "/")
+	if len(result) != 2 {
+		fmt.Println("invalid format: repository")
+		os.Exit(1)
+	}
+	return result[0], result[1]
+}
+
+func ghView(owner string, name string) {
+	args := []string{
+		"api", "graphql",
+		"-F", fmt.Sprintf("owner=%s", owner),
+		"-F", fmt.Sprintf("name=%s", name),
+		"-f", fmt.Sprintf("query=%s", `query($name: String!, $owner: String!) {
+			repository(name: $name, owner: $owner) {
+				name
+				pushedAt
+				openGraphImageUrl
+				refs(first: 10, refPrefix: "refs/heads/") {
+					nodes {
+						name
+						prefix
+						target {
+							... on Commit {
+								history(first: 1) {
+									nodes {
+										message
+										author {
+											name
+										}
+										committedDate
+									}
+								}
+							}
+						}
+					}
+          totalCount
+          pageInfo {
+            endCursor
+            hasNextPage
+            hasPreviousPage
+            startCursor
+          }
+				}
+			}
+		}`),
+	}
+
+	stdOut, _, err := gh.Exec(args...)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	printJson(stdOut.String())
 }
 
 func printJson(str string) {
