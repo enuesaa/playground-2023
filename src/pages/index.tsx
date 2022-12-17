a56134ae168bb2ebbf86b779a93cdcbafdf4b92b
enuesaa <nsrciog@gmail.com>
2022-12-17 19:33:55 +0900

    feat: enable to generate links on build

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,35 @@
 import { Header } from '@/components/common/Header'
 import { Main } from '@/components/common/Main'
 import Link from 'next/link'
+import { LinkCard } from '@/components/common/LinkCard'
 
-export default function TopPage() {
+import { promises as fs } from 'node:fs'
+import path from 'node:path'
+
+type Props = {
+  projectNames: string[]
+}
+export default function TopPage({ projectNames }: Props) {
   return (
     <>
       <Header />
       <Main>
-        <ul>
-          <li>
-            <Link href={'/slideup'}>slideup</Link>
-          </li>
-          <li>
-            <Link href={'/stylingout'}>stylingout</Link>
-          </li>
-        </ul>
+        {projectNames.map(p => (
+          <LinkCard href={`/projects/${p}`} name={p} />
+        ))}
       </Main>
     </>
   )
+}
+
+export const getStaticProps = async () => {
+  const dir = path.join(process.cwd(), 'src/pages/projects')
+  const filenames = await fs.readdir(dir)
+  const projectNames = filenames.map(n => n.replace('.tsx', ''))
+
+  return {
+    props: {
+      projectNames,
+    },
+  }
 }
