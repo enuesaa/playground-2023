3ab4e2e5a03ee195bfbf09a41e43404f1b2d578e
enuesaa <nsrciog@gmail.com>
2022-12-30 23:23:05 +0900

    fix(packedcss): add ts-ignore to jsx runtime code ... find and load type file later ...

--- src/lib/packedcss/jsx-dev-runtime.ts
+++ src/lib/packedcss/jsx-dev-runtime.ts
@@ -1,9 +1,11 @@
-import * as reactJsxRuntime from 'react/jsx-dev-runtime'
+import * as ReactJsxDevRuntime from 'react/jsx-dev-runtime'
 
 export function jsxDEV(type: any, props: any, key: any, isStaticChildren: any, source: any, self: any) {
   if (!props.hasOwnProperty('css')) {
-    return reactJsxRuntime.jsxDEV(type, props, key, isStaticChildren, source, self)
+    // @ts-ignore
+    return ReactJsxDevRuntime.jsxDEV(type, props, key, isStaticChildren, source, self)
   }
 
-  return reactJsxRuntime.jsxDEV(type, props, key, isStaticChildren, source, self)
+  // @ts-ignore
+  return ReactJsxDevRuntime.jsxDEV(type, props, key, isStaticChildren, source, self)
 }
--- src/lib/packedcss/jsx-runtime.ts
+++ src/lib/packedcss/jsx-runtime.ts
@@ -1,12 +1,14 @@
-import * as reactJsxRuntime from 'react/jsx-runtime'
+import * as ReactJsxRuntime from 'react/jsx-runtime'
 
 /**
  * @see https://github.com/emotion-js/emotion/blob/main/packages/react/src/jsx-runtime.js
  */
 export const jsx = (type: any, props: any, key: any) => {
   if (!props.hasOwnProperty('css')) {
-    return reactJsxRuntime.jsx(type, props, key)
+    // @ts-ignore
+    return ReactJsxRuntime.jsx(type, props, key)
   }
 
-  return reactJsxRuntime.jsx(type, props, key)
+  // @ts-ignore
+  return ReactJsxRuntime.jsx(type, props, key)
 }
