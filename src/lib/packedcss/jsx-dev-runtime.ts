9d06e1403d34ea70804d8e70763f6246a744cc4d
enuesaa <nsrciog@gmail.com>
2022-12-30 23:02:34 +0900

    feat(packedcss): add jsxDEV runtime

--- src/lib/packedcss/jsx-dev-runtime.ts
+++ src/lib/packedcss/jsx-dev-runtime.ts
@@ -1,1 +1,9 @@
-export { jsx as jsxDEV } from '@/lib/packedcss/jsx-runtime'
+import * as reactJsxRuntime from 'react/jsx-dev-runtime'
+
+export function jsxDEV(type: any, props: any, key: any, isStaticChildren: any, source: any, self: any) {
+  if (!props.hasOwnProperty('css')) {
+    return reactJsxRuntime.jsxDEV(type, props, key, isStaticChildren, source, self)
+  }
+
+  return reactJsxRuntime.jsxDEV(type, props, key, isStaticChildren, source, self)
+}
