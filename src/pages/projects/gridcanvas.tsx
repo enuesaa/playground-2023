7dfdeba6cfb2c16f3515cb81b9cd16761ac45a7f
enuesaa <nsrciog@gmail.com>
2022-12-31 12:22:01 +0900

    feat(gridcanvas): add component

--- src/pages/projects/gridcanvas.tsx
+++ src/pages/projects/gridcanvas.tsx
@@ -1,6 +1,7 @@
 import { Header } from '@/components/common/Header'
 import { Main } from '@/components/common/Main'
 import { Title } from '@/components/common/Title'
+import { Canvas } from '@/components/gridcanvas/Canvas'
 
 export default function TopPage() {
   return (
@@ -8,6 +9,7 @@ export default function TopPage() {
       <Header />
       <Main>
         <Title name='glidcanvas' />
+        <Canvas />
       </Main>
     </>
   )
