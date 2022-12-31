dd6ccb76c2638007c3b5508d2f4c24fd086c9c4b
enuesaa <nsrciog@gmail.com>
2022-12-31 13:54:27 +0900

    feat(gridcanvas): add basic shape

--- src/components/gridcanvas/Canvas.tsx
+++ src/components/gridcanvas/Canvas.tsx
@@ -1,3 +1,14 @@
+
 export const Canvas = () => {
-  return (<></>)
+  /** @see https://developer.mozilla.org/ja/docs/Web/SVG/Tutorial/Paths */
+
+  return (
+    <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 36 36'>
+      <path d="M 10 10 H 90 V 90 H 10 L 10 10" />
+      <circle cx="10" cy="10" r="2" fill="red" />
+      <circle cx="90" cy="90" r="2" fill="red" />
+      <circle cx="90" cy="10" r="2" fill="red" />
+      <circle cx="10" cy="90" r="2" fill="red" />
+    </svg>
+  )
 }
