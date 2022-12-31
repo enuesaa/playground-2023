533914b489d654056d2928816f39e449077281f8
enuesaa <nsrciog@gmail.com>
2022-12-31 18:13:18 +0900

    fix(packedcss): do not use jsx on jsx-runtime, ... instead use jsx or jsxDEV

--- src/lib/packedcss/jsx-dev-runtime.ts
+++ src/lib/packedcss/jsx-dev-runtime.ts
@@ -1,5 +1,15 @@
 import * as ReactJsxDevRuntime from 'react/jsx-dev-runtime'
-import { Styled } from './styled'
+
+const serialize = (className: string, styleObject: Record<string, string>) => {
+  const styleString = Object.keys(styleObject).reduce((prev: string, k) => {
+    const v = styleObject[k]
+    /** @see https://stackoverflow.com/questions/63116039/camelcase-to-kebab-case */
+    const kebabk = k.replace(/([a-z])([A-Z])/g, '$1-$2').toLowerCase()
+    return `${prev}${kebabk}:${v};`
+  }, '')
+
+  return `.${className}{ ${styleString} }`
+}
 
 export function jsxDEV(type: any, props: Record<string, any>, key: any, isStaticChildren: any, source: any, self: any) {
   if (!props.hasOwnProperty('css')) {
@@ -7,17 +17,21 @@ export function jsxDEV(type: any, props: Record<string, any>, key: any, isStatic
     return ReactJsxDevRuntime.jsxDEV(type, props, key, isStaticChildren, source, self)
   }
 
-  const trunckedProps = {
-    proper: {} as Record<string, any>,
-    packed: { type, css: props.css }
-  }
+  const properProps: Record<string, any> = {}
   Object.keys(props).map(k => {
     if (k !== 'css') {
-      trunckedProps.proper[k] = props[k]
+      properProps[k] = props[k]
     }
   })
-  console.log(trunckedProps)
+  
+  // @ts-ignore
+  const styled = ReactJsxDevRuntime.jsxDEV('style', {
+    children: serialize(props.css.className, props.css.style),
+  })
+  properProps['className'] = props.css.className
+  // @ts-ignore
+  const wrapped = ReactJsxDevRuntime.jsxDEV(type, properProps, key, isStaticChildren, source, self)
 
   // @ts-ignore
-  return ReactJsxDevRuntime.jsxDEV(Styled, trunckedProps, key, isStaticChildren, source, self)
+  return ReactJsxDevRuntime.jsxDEV('div', { children: [styled, wrapped] })
 }
