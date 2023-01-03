07a45d9322b93bced5068dd074bb5661b30eeb43
enuesaa <nsrciog@gmail.com>
2023-01-03 18:57:44 +0900

    feat(tauri-nextjs-tutorial): do not open devtool

--- tauri-nextjs-tutorial/src/main.rs
+++ tauri-nextjs-tutorial/src/main.rs
@@ -1,16 +1,5 @@
-use tauri::Manager;
-
 fn main() {
   tauri::Builder::default()
-    .setup(|app| {
-      #[cfg(debug_assertions)] // only include this code on debug builds
-      {
-        let window = app.get_window("main").unwrap();
-        window.open_devtools();
-        window.close_devtools();
-      }
-      Ok(())
-    })
     .invoke_handler(tauri::generate_handler![greet])
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
