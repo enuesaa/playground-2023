60f07d92a65dd58e8bfd4a2c7479fee8d81316b1
enuesaa <nsrciog@gmail.com>
2023-01-03 15:11:51 +0900

    feat(tauri-nextjs-tutorial): open devtool

--- tauri-nextjs-tutorial/src-tauri/src/main.rs
+++ tauri-nextjs-tutorial/src-tauri/src/main.rs
@@ -3,8 +3,19 @@
   windows_subsystem = "windows"
 )]
 
+use tauri::Manager;
+
 fn main() {
   tauri::Builder::default()
+    .setup(|app| {
+      #[cfg(debug_assertions)] // only include this code on debug builds
+      {
+        let window = app.get_window("main").unwrap();
+        window.open_devtools();
+        window.close_devtools();
+      }
+      Ok(())
+    })
     .invoke_handler(tauri::generate_handler![greet])
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
@@ -12,6 +23,5 @@ fn main() {
 
 #[tauri::command]
 fn greet(name: &str) -> String {
-  println!("Hello from println, {}!", name);
   format!("Hello, {}!", name)
 }
