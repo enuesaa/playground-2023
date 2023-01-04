be7c2dd51a210540803cc0e04bdb5a598da12165
enuesaa <nsrciog@gmail.com>
2023-01-04 23:14:24 +0900

    feat(nextjs-mui): sidebar

--- nextjs-mui/components/Sidebar.tsx
+++ nextjs-mui/components/Sidebar.tsx
@@ -1,9 +1,22 @@
-import { Drawer } from '@mui/material'
+import { Drawer, List, ListItem, ListItemButton, ListItemIcon, ListItemText } from '@mui/material'
+import InboxIcon from '@mui/icons-material/MoveToInbox'
+import MailIcon from '@mui/icons-material/Mail'
 
 export const Sidebar = () => {
+  /* @see https://mui.com/material-ui/react-drawer/ */
+
   return (
-    <Drawer anchor='left' open={true}>
-      a
+    <Drawer anchor='left' open={true} variant="persistent">
+      <List>
+        {['Inbox', 'Starred'].map((text, index) => (
+          <ListItem key={text}>
+            <ListItemButton>
+              <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
     </Drawer>
   )
 }
