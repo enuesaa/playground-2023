8f61363afc284072a67eadda41e549ebf5d4fd8a
enuesaa <nsrciog@gmail.com>
2023-01-04 19:45:17 +0900

    feat(tauri-wry): save histories

--- tauri-wry/src/main.rs
+++ tauri-wry/src/main.rs
@@ -7,21 +7,26 @@ use wry::{
   },
   webview::WebViewBuilder,
 };
+enum UserEvent {
+  Navigation(String),
+}
 
 fn main() -> wry::Result<()> {
-  let event_loop = EventLoop::new();
+  // https://github.com/tauri-apps/wry/blob/dev/examples/navigation_event.rs
+  let event_loop: EventLoop<UserEvent> = EventLoop::with_user_event();
+  let proxy = event_loop.create_proxy();
   let window = WindowBuilder::new()
     .with_title("wry")
     .with_inner_size(LogicalSize::new(700, 1000))
     .build(&event_loop)?;
   
   let user_agent_string = "Mozilla/5.0 (iPhone; CPU iPhone OS 14_5 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.1 Mobile/15E148 Safari/604.1";
-  
+  let mut histories: Vec<String> = Vec::new();
+
   let webview = WebViewBuilder::new(window)?
     .with_user_agent(&user_agent_string)
-    .with_navigation_handler(move |uri| {
-      println!("{:?}", uri);
-      true
+    .with_navigation_handler(move |uri: String| {
+      proxy.send_event(UserEvent::Navigation(uri.clone())).is_ok() || true
     })
     .with_url("https://yahoo.co.jp")?
     .build()?;
@@ -37,10 +42,13 @@ fn main() -> wry::Result<()> {
       Event::WindowEvent {
         event: WindowEvent::CloseRequested,
         ..
-      } => *control_flow = ControlFlow::Exit,
-      // Event::NewEvents(StartCause::WaitCancelled{..}) => (),
-      // Event::MainEventsCleared => (),
-      // Event::RedrawEventsCleared => (),
+      } => {
+        println!("{:?}", histories);
+        *control_flow = ControlFlow::Exit
+      },
+      Event::UserEvent(UserEvent::Navigation(uri)) => {
+        histories.push(uri.clone())
+      },
       _ => (),
     }
   });
