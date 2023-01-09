8ca8e96347433f353bc6f7584037d5b2169b3d09
enuesaa <nsrciog@gmail.com>
2023-01-09 22:40:10 +0900

    chore(packedcss): add memo

--- src/components/packedcss/Card.tsx
+++ src/components/packedcss/Card.tsx
@@ -3,9 +3,15 @@ import { usePacked } from '@/lib/packedcss/usePacked'
 export const Card = () => {
   const packed = usePacked()
 
-  // const customCard = packed.card.with({
-  //   borderRadius: '5px',
-  // })
+  /**
+   * const customCard = packed.card.with({
+   *   borderRadius: '5px',
+   * })
+   * 
+   * or
+   * 
+   * const customCard = packed.card * 2 // meaning like width * 2
+   */
 
   // return class id
   const customCard = packed.card()
