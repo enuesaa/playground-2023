4d1fc998779e2e4e7c83c626a28473b44c6d8cd8
enuesaa <nsrciog@gmail.com>
2023-01-13 01:04:40 +0900

    feat(chakraui): add Accordion

--- chakraui/app/layout.tsx
+++ chakraui/app/layout.tsx
@@ -1,11 +1,17 @@
+"use client"
 import RootStyleRegistry from './emotion'
 import './global.css'
+import { ChakraProvider } from '@chakra-ui/react'
 
 export default function RootLayout({ children }: { children: JSX.Element }) {
   return (
     <html lang="ja">
       <body>
-        <RootStyleRegistry>{children}</RootStyleRegistry>
+        <RootStyleRegistry>
+          <ChakraProvider>
+            {children}
+          </ChakraProvider>
+        </RootStyleRegistry>
       </body>
     </html>
   )
--- chakraui/app/page.tsx
+++ chakraui/app/page.tsx
@@ -1,6 +1,14 @@
 'use client'
 /** @jsxImportSource @emotion/react */
 import { css } from '@emotion/react'
+import {
+  Accordion,
+  AccordionItem,
+  AccordionButton,
+  AccordionPanel,
+  AccordionIcon,
+  Box,
+} from '@chakra-ui/react'
 
 export default function Page() {
   const style = {
@@ -10,5 +18,41 @@ export default function Page() {
   }
 
   // @ts-ignore
-  return <h1 css={style.h1}>nextjs13</h1>
+  return (
+    <Accordion>
+      <AccordionItem>
+        <h2>
+          <AccordionButton>
+            <Box as="span" flex='1' textAlign='left'>
+              Section 1 title
+            </Box>
+            <AccordionIcon />
+          </AccordionButton>
+        </h2>
+        <AccordionPanel pb={4}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+          tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+          veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+          commodo consequat.
+        </AccordionPanel>
+      </AccordionItem>
+
+      <AccordionItem>
+        <h2>
+          <AccordionButton>
+            <Box as="span" flex='1' textAlign='left'>
+              Section 2 title
+            </Box>
+            <AccordionIcon />
+          </AccordionButton>
+        </h2>
+        <AccordionPanel pb={4}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+          tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+          veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+          commodo consequat.
+        </AccordionPanel>
+      </AccordionItem>
+    </Accordion>
+  )
 }
