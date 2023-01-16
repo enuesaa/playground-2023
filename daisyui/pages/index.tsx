9fbb7ca871ee348e022c3ecbf3b32c3b0b237940
enuesaa <nsrciog@gmail.com>
2023-01-16 23:43:53 +0900

    feat(daisyui): add sample codes

--- daisyui/pages/index.tsx
+++ daisyui/pages/index.tsx
@@ -1,7 +1,11 @@
 import type { NextPage } from 'next'
 
 const TopPage: NextPage = () => {
-  return <>nextjs</>
+  return (
+    <>
+      <button className="btn">Hello daisyUI</button>
+    </>
+  )
 }
 
 export default TopPage
