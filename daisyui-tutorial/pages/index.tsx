4cb1a0558da2ac7d6c6467095c6741dc50ac3ec1
enuesaa <nsrciog@gmail.com>
2023-01-17 00:11:32 +0900

    refactor(daisyui-tutorial): next.js config

--- daisyui-tutorial/pages/index.tsx
+++ daisyui-tutorial/pages/index.tsx
@@ -1,11 +1,12 @@
 import type { NextPage } from 'next'
 import { Sidebar } from '../components/Sidebar'
+import { Header } from '../components/Header'
 
 const TopPage: NextPage = () => {
   return (
     <>
+      <Header />
       <Sidebar />
-      <button className="btn">Hello daisyUI</button>
     </>
   )
 }
