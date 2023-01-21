354ab1380e95a3984577dc09c96a1a40f6309f27
enuesaa <nsrciog@gmail.com>
2023-01-21 19:53:50 +0900

    feat: enable to load imdb data

--- app/main.py
+++ app/main.py
@@ -1,2 +1,6 @@
+from keras.datasets import imdb
+
 def app():
-  print('a')
+  # https://qiita.com/hkambe/items/8c56ca8f0bbb4f895dee
+  (train_data, train_labels), (test_data, test_labels) = imdb.load_data(num_words=10000)
+  print(train_data[0])
