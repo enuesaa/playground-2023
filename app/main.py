b492b73ae6cefbfb89ad65e7ce4bb24be0dbd298
enuesaa <nsrciog@gmail.com>
2023-01-27 23:57:53 +0900

    style: change indent level from 2 spaces to 4 spaces

--- app/main.py
+++ app/main.py
@@ -1,8 +1,9 @@
 from keras.datasets import imdb
 from app.doc2vec import main
 
+
 def app():
-  # https://qiita.com/hkambe/items/8c56ca8f0bbb4f895dee
-  # (train_data, train_labels), (test_data, test_labels) = imdb.load_data(num_words=10000)
-  # print(train_data[0])
-  main()
+    # https://qiita.com/hkambe/items/8c56ca8f0bbb4f895dee
+    # (train_data, train_labels), (test_data, test_labels) = imdb.load_data(num_words=10000)
+    # print(train_data[0])
+    main()
