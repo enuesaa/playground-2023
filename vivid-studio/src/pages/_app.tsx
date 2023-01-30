310cba181c8bdbc17019944ac3180e859d73ac0a
enuesaa <nsrciog@gmail.com>
2023-01-30 23:45:40 +0900

    feat(vivid-studio): add vivid's initialize code

--- vivid-studio/src/pages/_app.tsx
+++ vivid-studio/src/pages/_app.tsx
@@ -2,10 +2,19 @@ import type { AppProps } from 'next/app'
 import 'modern-css-reset/dist/reset.min.css'
 import '../styles/global.css'
 
+/** @see https://docs.vivid.lol/ */
+if (
+  typeof window !== "undefined" &&
+  process.env.NODE_ENV === "development"
+  // && /VIVID_ENABLED=true/.test(document.cookie)
+) {
+  import("vivid-studio").then((v) => v.run());
+  import("vivid-studio/style.css");
+}
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Component {...pageProps} />
     </>
   )
 }
