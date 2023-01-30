450df3844fea22cef7d2bd0e930b067e9853f046
enuesaa <nsrciog@gmail.com>
2023-01-31 02:18:49 +0900

    feat(radixui): emotion で styling した

--- radixui/src/components/demo/PopoverDemo.tsx
+++ radixui/src/components/demo/PopoverDemo.tsx
@@ -1,14 +1,32 @@
-import * as Popover from '@radix-ui/react-popover'
-import styles from '@/components/demo/styles.module.css'
+import { Root, Trigger, Content, Portal, Arrow } from '@radix-ui/react-popover'
+import { css } from '@emotion/react'
 
-export const PopoverDemo = () => (
-  <Popover.Root>
-    <Popover.Trigger className={styles.PopoverTrigger}>Show info</Popover.Trigger>
-    <Popover.Portal>
-      <Popover.Content className={styles.PopoverContent}>
-        Some content
-        <Popover.Arrow className={styles.PopoverAllow} />
-      </Popover.Content>
-    </Popover.Portal>
-  </Popover.Root>
-)
+export const PopoverDemo = () => {
+  const styles = {
+    trigger: css({
+      background: '#fafafa',
+      borderRadius: '5px',
+    }),
+    content: css({
+      borderRadius: '5px',
+      padding: '20px',
+      width: '260px',
+      background: '#fafafa',
+    }),
+    arrow: css({
+      fill: '#fafafa',
+    })
+  }
+
+  return (
+    <Root>
+      <Trigger css={styles.trigger}>Show info</Trigger>
+      <Portal>
+        <Content css={styles.content}>
+          Some content
+          <Arrow css={styles.arrow} />
+        </Content>
+      </Portal>
+    </Root>
+  )
+}
