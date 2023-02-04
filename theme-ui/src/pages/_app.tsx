d2c175e921b65008a4b47c847c481fc9c1b9de55
enuesaa <nsrciog@gmail.com>
2023-02-04 14:29:04 +0900

    feat(theme-ui): gloabal style を theme-ui に寄せた

--- theme-ui/src/pages/_app.tsx
+++ theme-ui/src/pages/_app.tsx
@@ -1,24 +1,14 @@
 import type { AppProps } from 'next/app'
 import 'modern-css-reset/dist/reset.min.css'
-import { Global } from '@emotion/react'
-import { globalStyle } from '@/styles/global'
 import { ThemeProvider } from 'theme-ui'
 import { theme } from '@/styles/theme'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Component {...pageProps} />
       <ThemeProvider theme={theme}>
-        <h1
-          sx={{
-            color: 'primary',
-            fontFamily: 'heading',
-          }}>
-          Hello
-        </h1>
+        <Component {...pageProps} />
       </ThemeProvider>
-      <Global styles={globalStyle} />
     </>
   )
 }
--- theme-ui/src/styles/theme.ts
+++ theme-ui/src/styles/theme.ts
@@ -11,4 +11,23 @@ export const theme: Theme = {
     background: '#fff',
     primary: '#33e',
   },
+  styles: {
+    root: {
+      'html,body': {
+        padding: '0',
+        margin: '0',
+        fontFamily: '-apple-system, BlinkMacSystemFont, Segoe UI, Roboto, sans-serif',
+      },
+      a: {
+        color: 'inherit',
+        textDecoration: 'none',
+      },
+      '*': {
+        boxSizing: 'border-box',
+      },
+      body: {
+        background: '#1a1a1a',
+      }
+    }
+  }
 }
