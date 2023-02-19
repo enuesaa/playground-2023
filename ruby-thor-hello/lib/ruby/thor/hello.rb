2109c0c9d11568630bfd0d9911ef34009e757972
enuesaa <nsrciog@gmail.com>
2023-02-20 01:02:01 +0900

    feat(ruby-thor-hello): run command like `ruby-thor-hello`

---
+++ ruby-thor-hello/lib/ruby/thor/hello.rb
@@ -0,0 +1,10 @@
+require "ruby/thor/hello/version"
+
+module Ruby
+  module Thor
+    module Hello
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
---
+++ ruby-thor-hello/lib/ruby/thor/hello/version.rb
@@ -0,0 +1,7 @@
+module Ruby
+  module Thor
+    module Hello
+      VERSION = "0.1.0"
+    end
+  end
+end
