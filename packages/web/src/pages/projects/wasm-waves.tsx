e1b0c4d86467237b6082b40020498410205c3856
enuesaa <nsrciog@gmail.com>
2023-02-19 19:25:31 +0900

    chore(docs): around wasm-waves

--- packages/web/src/pages/projects/wasm-waves.tsx
+++ packages/web/src/pages/projects/wasm-waves.tsx
@@ -3,6 +3,10 @@ import { Main } from '@/components/common/Main'
 import { Title } from '@/components/common/Title'
 
 export default function TopPage() {
+  /**
+   * イメージは rive.
+   * cursor の動きに合わせてデザインが変わったり svg のアニメーションが効いたりする
+   */
   return (
     <>
       <Header />
