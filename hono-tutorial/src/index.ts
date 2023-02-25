dad0c46f754c5b576473ba20248ea5145b3f25d6
enuesaa <nsrciog@gmail.com>
2023-02-25 15:42:16 +0900

    feat(hono): add routes

--- hono-tutorial/src/index.ts
+++ hono-tutorial/src/index.ts
@@ -3,5 +3,7 @@ import { Hono } from 'hono'
 
 const app = new Hono()
 app.get('/', (c) => c.text('Hello Hono!'))
+app.get('/aaa', c => c.json({ path: 'aaa' }))
+app.get('/bbb', c => c.json({ path: 'bbb' }))
 
 serve(app)
