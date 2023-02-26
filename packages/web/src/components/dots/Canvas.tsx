2718bf679e0ae32e0496f04de3c8dcf80449f40d
enuesaa <nsrciog@gmail.com>
2023-02-26 19:35:48 +0900

    fix: specify key props on map elements

--- packages/web/src/components/dots/Canvas.tsx
+++ packages/web/src/components/dots/Canvas.tsx
@@ -23,8 +23,8 @@ export const Canvas = () => {
     <>
       <svg width='600' height='600'>
         <line x1='0' y1='0' x2='600' y2='600' stroke='#ff6633' />
-        {points.map(p => (
-          <circle cx={p.x} cy={p.y} r='1' fill='#ff6633' />
+        {points.map((p, i) => (
+          <circle key={i} cx={p.x} cy={p.y} r='1' fill='#ff6633' />
         ))}
       </svg>
       <button onClick={handleGreet}>greet</button>
