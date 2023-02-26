6d58110603cdb8f7c2a7a293967acd5638c5e1a1
enuesaa <nsrciog@gmail.com>
2023-02-26 20:12:56 +0900

    feat: rust でランダムなxyを生成し返す処理を途中まで実装

--- packages/waveslib/src/lib.rs
+++ packages/waveslib/src/lib.rs
@@ -1,8 +1,26 @@
 use wasm_bindgen::prelude::*;
+use serde::Serialize;
+use rand::SeedableRng;
 
 #[wasm_bindgen]
 pub fn greet(name: &str) -> String {
     format!("Hello, {}!", name)
+}
+
+#[derive(Serialize)]
+struct Points {
+    x: u16,
+    y: u16,
+}
+#[wasm_bindgen]
+pub fn generate_points(count: u8) -> Result<JsValue, JsValue> {
+    let mut rng: rand::rngs::StdRng = SeedableRng::from_seed([13; 32]);
+    
+    let mut ret = vec![];
+    for _i in 1..count {
+        ret.push(Points { x: rng.gen::<u16>(), y: rng.gen::<u16>() });
+    };
+    Ok(serde_wasm_bindgen::to_value(&ret)?)
 }
 
 #[test]
