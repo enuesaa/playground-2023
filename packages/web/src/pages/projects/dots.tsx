9775115f420a394d189db36944d0019f7546c1a6
enuesaa <nsrciog@gmail.com>
2023-02-26 15:31:18 +0900

    feat: 簡単な svg を表示できるようにした

--- packages/web/src/pages/projects/dots.tsx
+++ packages/web/src/pages/projects/dots.tsx
@@ -9,6 +9,10 @@ export default function TopPage() {
       <Header />
       <Main>
         <Title name='dots' />
+        <svg width='200' height='200'>
+          <line x1='0' y1='0' x2='200' y2='200' stroke='#ff6633' />
+          <circle cx='100' cy='100' r='1' fill='#ff6633' />
+        </svg>
       </Main>
     </>
   )
