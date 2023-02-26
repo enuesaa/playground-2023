bfa357687a1fe175ade335fa263092f1480e279e
enuesaa <nsrciog@gmail.com>
2023-02-27 00:26:48 +0900

    refactor(waveslib): wasm_bindgen の 実行環境がwasmである関係でテストがしにくいことが分かったので、実処理を service として切り出す

--- packages/waveslib/src/lib.rs
+++ packages/waveslib/src/lib.rs
@@ -1,31 +1,17 @@
 use wasm_bindgen::prelude::*;
-use serde::Serialize;
-use rand::SeedableRng;
-use rand::rngs::StdRng;
-use rand::prelude::*;
+
+pub mod services;
+use crate::services::points::Points;
 
 #[wasm_bindgen]
 pub fn greet(name: &str) -> String {
     format!("Hello, {}!", name)
 }
 
-#[derive(Serialize)]
-struct Points {
-    x: u8,
-    y: u8,
-}
 #[wasm_bindgen]
 pub fn generate_points(count: usize) -> Result<JsValue, JsValue> {
-    let mut rng: StdRng = SeedableRng::seed_from_u64(99);
-    
-    let mut ret = Vec::with_capacity(count);
-    for _ in 0..count {
-        ret.push(Points {
-            x: rng.gen::<u8>(),
-            y: rng.gen::<u8>(),
-        });
-    };
-    Ok(serde_wasm_bindgen::to_value(&ret)?)
+    let ret = Points::from_count(count);
+    Ok(serde_wasm_bindgen::to_value(&ret.values)?)
 }
 
 #[test]
