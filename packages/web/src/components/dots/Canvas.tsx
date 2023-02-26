cf2420eb4db483ca18b2ce95de91a5baa1d538f1
enuesaa <nsrciog@gmail.com>
2023-02-27 00:33:01 +0900

    feat(dots): generate points by wasm

--- packages/web/src/components/dots/Canvas.tsx
+++ packages/web/src/components/dots/Canvas.tsx
@@ -8,40 +8,23 @@ type Point = {
 }
 
 export const Canvas = () => {
-  const points: Point[] = [];
-  for (let i = 0; i < 1000; i++) {
-    points.push({ x: Math.random() * 600 , y: Math.random() * 600 })
-  }
+  const points: Point[] = generate_points(1000)
 
   const handleGreet: MouseEventHandler<HTMLButtonElement> = (e) => {
     e.preventDefault()
     const res = greet('aa')
     console.log(res)
   }
-  const handleGeneratePoints: MouseEventHandler<HTMLButtonElement> = (e) => {
-    e.preventDefault()
-    const res = generate_points(1000)
-    console.log(res)
-  }
-
-  /**
-   * @todo react strict mode をどう handle するか。
-   * 
-   * 部分的に無効化したいが、手立てがないように見える。
-   * プロジェクト全体で react strict mode を無効化するのは、おかしな話だと思うので、
-   * ランダム値を使う Component が何かしら対処すべきだと思う。Seed 値のような仕組みを作るか。。
-   */
 
   return (
     <>
-      <svg width='600' height='600'>
-        <line x1='0' y1='0' x2='600' y2='600' stroke='#ff6633' />
+      <svg width='255' height='255'>
+        <line x1='0' y1='0' x2='255' y2='255' stroke='#ff6633' />
         {points.map((p, i) => (
           <circle key={i} cx={p.x} cy={p.y} r='1' fill='#ff6633' />
         ))}
       </svg>
       <button onClick={handleGreet}>greet</button>
-      <button onClick={handleGeneratePoints}>generatePoints</button>
     </>
   )
 }
