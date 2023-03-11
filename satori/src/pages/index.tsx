cd91608ee67583563c8b9bdb917483e93051f22e
enuesaa <nsrciog@gmail.com>
2023-03-12 00:12:46 +0900

    feat(satori): svg を生成し出力してみる

--- satori/src/pages/index.tsx
+++ satori/src/pages/index.tsx
@@ -1,11 +1,34 @@
 import { Header } from '@/components/common/Header'
 import { Main } from '@/components/common/Main'
+import satori from 'satori'
+
 
 export default function TopPage() {
+  (async () => {
+    const svg = await satori(
+      <div style={{ color: 'black' }}>hello, world</div>,
+      {
+        width: 600,
+        height: 400,
+        fonts: [
+          {
+            name: 'Roboto',
+            data: await fetch("/Roboto-Medium.ttf").then(res => res.arrayBuffer()),
+            weight: 400,
+            style: 'normal',
+          },
+        ],
+      },
+    )
+    console.log(svg)
+  })()
+
   return (
     <>
       <Header />
-      <Main></Main>
+      <Main>
+        
+      </Main>
     </>
   )
 }
