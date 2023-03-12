0321b315325e9566c71f55fb15517a2fca21ddb7
enuesaa <nsrciog@gmail.com>
2023-03-12 15:04:06 +0900

    feat(packedcss): minimum spec

---
+++ packages/packedcss/src/index.ts
@@ -0,0 +1,25 @@
+class Packed {
+  public style: Record<string, string>;
+
+  constructor() {
+    this.style = {}
+  }
+
+  width(value: string): this {
+    this.style['width'] = value;
+    return this
+  }
+
+  darken(): this {
+    this.style['background'] = '#1a1a1a';
+    this.style['color'] = '#fafafa';
+    return this
+  }
+
+  radii(): this {
+    this.style['borderRadius'] = '10px';
+    return this
+  }
+}
+
+export const packed = (): Packed => new Packed()
---
+++ packages/packedcss/tests/emotion.spec.ts
@@ -0,0 +1,14 @@
+import { packed } from '../src/index'
+
+describe('packed', () => {
+  it('packed', () => {
+    expect(
+      packed().width('100%').darken().radii().style
+    ).toStrictEqual({
+      width: '100%',
+      background: '#1a1a1a',
+      color: '#fafafa',
+      borderRadius: '10px',
+    })
+  })
+})
