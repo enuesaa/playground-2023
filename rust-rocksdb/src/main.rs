d560201416776b7f4070abcd9b9b0545a57ecb89
enuesaa <nsrciog@gmail.com>
2023-03-31 02:36:37 +0900

    feat(rust-rocksdb): list keys

--- rust-rocksdb/src/main.rs
+++ rust-rocksdb/src/main.rs
@@ -4,12 +4,21 @@ fn main() {
     let db = DB::open_default("rocksdb").unwrap();
 
     // set
-    // let _ = db.put(b"aaa", b"value");
+    // let _ = db.put(b"aaa", b"bbb");
 
     // get
-    let res = db.get(b"aaa");
-    if let Ok(Some(value)) = res {
-        let valstr = String::from_utf8(value).unwrap();
-        println!("{:?}", valstr);
-    }
+    // let res = db.get(b"aaa");
+    // if let Ok(Some(value)) = res {
+    //     let valstr = String::from_utf8(value).unwrap();
+    //     println!("{:?}", valstr);
+    // }
+
+    let mut iter = db.raw_iterator();
+    iter.seek(b"a");
+
+    if iter.valid() {
+        if let (Some(key), Some(value)) = (iter.key(), iter.value()) {
+            println!("{:?} {:?}", String::from_utf8(Vec::from(key)), String::from_utf8(Vec::from(value)));
+        }
+    }; 
 }
