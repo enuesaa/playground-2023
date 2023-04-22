cd224f85e462a155a77ca5c0c838c5028bea0338
enuesaa <nsrciog@gmail.com>
2023-04-23 00:29:43 +0900

    feat(webcontainer): 思ったよりもすごかった

--- webcontainer/README.md
+++ webcontainer/README.md
@@ -4,3 +4,4 @@ NodeJSライクなAPIをブラウザで扱えるらしい？
 ## Links
 - https://webcontainers.io/guides/quickstart
 - https://www.publickey1.jp/blog/23/webwebassemblynodejswebcontainerapihttpnodejs_cli.html
+- [API Reference](https://webcontainers.io/api#webcontainerprocess)
--- webcontainer/src/components/container/Container.tsx
+++ webcontainer/src/components/container/Container.tsx
@@ -1,5 +1,11 @@
 import { WebContainer } from "@webcontainer/api"
 
+const run = async (container: WebContainer, name: string, args: string[]): Promise<string> => {
+  const ps = await container.spawn(name, args);
+  const out = await ps.output.getReader().read()
+  return out.value ?? ''
+}
+
 export const Container = () => {
 
   if (typeof window !== 'object') {
@@ -8,9 +14,23 @@ export const Container = () => {
 
   (async () => {
     const container = await WebContainer.boot()
-    const installProcess = await container.spawn('npm', ['install']);
-    const code = await installProcess.exit
-    console.log(code)
+    console.log(await run(container, 'node', ['--version']))
+    console.log(await run(container, 'which', ['node']))
+    console.log(await run(container, 'yarn', ['--version']))
+    console.log(await run(container, 'echo', ['a']))
+    console.log(await run(container, 'ls', ['--l']))
+    console.log(await run(container, 'pwd', []))
+    console.log(await run(container, 'ls', ['/usr/local/bin']))
+    console.log(await run(container, 'printenv', []))
+    console.log(await run(container, 'yarn', ['install']))
+    console.log(await run(container, 'yarn', ['add', '--dev', 'prettier']))
+
+    await new Promise(resolve => setTimeout(resolve, 3000))
+
+    const files = await container.fs.readdir('./')
+    console.log(files)
+    const files2 = await container.fs.readdir('./node_modules')
+    console.log(files2)
   })()
 
   return (<></>)
