7ab3fbb12fb82bbdf563603cec28e36afdf0b428
enuesaa <nsrciog@gmail.com>
2023-04-22 23:57:45 +0900

    feat(webcontainer): add security headers configuration

--- webcontainer/next.config.js
+++ webcontainer/next.config.js
@@ -4,6 +4,25 @@ const nextConfig = {
   experimental: {
     appDir: false,
   },
+  async headers() {
+    const securityHeaders = [
+      {
+        key: 'Cross-Origin-Embedder-Policy',
+        value: 'require-corp'
+      },
+      {
+        key: 'Cross-Origin-Opener-Policy',
+        value: 'same-origin'
+      }
+    ]
+
+    return [
+      {
+        source: '/:path*',
+        headers: securityHeaders,
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
