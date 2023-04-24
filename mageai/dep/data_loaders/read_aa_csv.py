ab7a577313aaaf6d08ce393727c673494c457d41
enuesaa <nsrciog@gmail.com>
2023-04-24 21:04:58 +0900

    feat(mageai): create pipeline

---
+++ mageai/dep/data_loaders/read_aa_csv.py
@@ -0,0 +1,11 @@
+from mage_ai.io.file import FileIO
+if 'data_loader' not in globals():
+    from mage_ai.data_preparation.decorators import data_loader
+if 'test' not in globals():
+    from mage_ai.data_preparation.decorators import test
+
+@data_loader
+def load_data_from_file(*args, **kwargs):
+    filepath = 'aa.csv'
+
+    return FileIO().load(filepath)
