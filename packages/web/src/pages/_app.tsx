53f95992f4179f5f7bb7db1d0f655468cadbe326
enuesaa <nsrciog@gmail.com>
2023-04-29 17:08:34 +0900

    Revert "feat: 試験的に react sketch canvas を入れた"
    This reverts commit a26d54d9fea701dcb0e8aaca80cd33df3c719b1e.

--- packages/web/src/pages/_app.tsx
+++ packages/web/src/pages/_app.tsx
@@ -5,7 +5,6 @@ import { Layout } from '@/components/common/Layout'
 import type { ReactElement, ReactNode } from 'react'
 import type { NextPage } from 'next'
 import { baseTheme } from '@/styles/theme'
-import { ReactSketchCanvas } from "react-sketch-canvas"
 
 export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
   getLayout?: (page: ReactElement) => ReactNode
@@ -15,22 +14,7 @@ type Props = AppProps & {
   Component: NextPageWithLayout
 }
 export default function App({ Component, pageProps }: Props) {
-  const styles = {
-    border: "0.0625rem solid #1a1a1a",
-    borderRadius: "0.25rem",
-  };
-  const getLayout = Component.getLayout ?? ((page) => (
-    <Layout>
-      {page}
-      <ReactSketchCanvas
-        style={styles}
-        width="600"
-        height="400"
-        strokeWidth={4}
-        strokeColor="red"
-      />
-    </Layout>)
-  )
+  const getLayout = Component.getLayout ?? ((page) => <Layout>{page}</Layout>)
 
   return (
     <>
