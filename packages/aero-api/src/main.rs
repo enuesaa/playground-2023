bcedf73f445bdcbd0a8814537f48201551a8c169
enuesaa <nsrciog@gmail.com>
2023-05-03 14:35:56 +0900

    feat(api): enable to upload file

--- packages/aero-api/src/main.rs
+++ packages/aero-api/src/main.rs
@@ -1,13 +1,17 @@
 use axum::{
-    routing::get,
+    routing::{get, post},
     Router,
+    extract::Multipart,
 };
 use std::net::SocketAddr;
+use std::fs;
+use dirs::home_dir;
 
 #[tokio::main]
 async fn main() {
     let app = Router::new()
-        .route("/", get(root));
+        .route("/", get(root))
+        .route("/upload", post(upload));
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
     axum::Server::bind(&addr)
@@ -18,4 +22,15 @@ async fn main() {
 
 async fn root() -> &'static str {
     "Hello, World!"
+}
+
+async fn upload(mut multipart: Multipart) {
+    while let Some(field) = multipart.next_field().await.unwrap() {
+        let name = field.name().unwrap().to_string();
+        let data = field.bytes().await.unwrap();
+
+        println!("Length of `{}` is {} bytes", name, data.len());
+        let path = home_dir().unwrap().join("tmp/a.jpeg");
+        fs::write(path, data).unwrap();
+    }
 }
