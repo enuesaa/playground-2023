7e29bd908f4da7eb09c1634ac3d80181f7b78670
enuesaa <nsrciog@gmail.com>
2023-05-13 23:59:54 +0900

    feat: go で wasm app を実行する環境を整えた

--- code-packages/fngo/README.md
+++ code-packages/fngo/README.md
@@ -2,4 +2,5 @@
 wasm app of golang.
 
 ## Links
+- https://buildersbox.corp-sansan.com/entry/2019/02/14/113000
 - https://cpp-learning.com/golang-webassembly/
--- code-packages/web/src/pages/index.tsx
+++ code-packages/web/src/pages/index.tsx
@@ -1,7 +1,13 @@
 import { Header } from '@/components/common/Header'
 import { Main } from '@/components/common/Main'
+import 'wasm_exec-ts'
 
 export default function Page() {
+  const go = new Go();
+  WebAssembly.instantiateStreaming(fetch("main.wasm"), go.importObject).then((result) => {
+      go.run(result.instance);
+  });
+
   return (
     <>
       <Header />
