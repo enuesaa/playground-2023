3a08691b930fb683ea83d830b969c847150b4de7
enuesaa <nsrciog@gmail.com>
2023-05-13 23:47:45 +0900

    chore: remove un-defined nextjs config on latest version of nextjs

--- packages/web/next.config.js
+++ packages/web/next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    appDir: false,
-  },
   output: 'export',
   distDir: 'dist',
   webpack: (config, { isServer }) => {
