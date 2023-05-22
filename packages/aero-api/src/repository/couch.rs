26909120875c916a2f5e56192e3ae5aad37e5748
enuesaa <nsrciog@gmail.com>
2023-05-23 02:21:20 +0900

    feat: create couch trait to mock couch repository... but it is not working. it may be something wrong with trait definition.

--- packages/aero-api/src/repository/couch.rs
+++ packages/aero-api/src/repository/couch.rs
@@ -4,6 +4,7 @@ use couch_rs::database::Database;
 use couch_rs::document::DocumentCollection;
 use serde_json::Value;
 use couch_rs::document::TypedCouchDocument;
+use async_trait::async_trait;
 
 #[derive(Clone)]
 pub struct CouchRepository {
@@ -19,39 +20,54 @@ impl CouchRepository {
             password: "admin".to_string(),
         }
     }
+}
 
-    pub fn client(&self) -> Client {
+#[async_trait]
+pub trait CouchTrait {
+    fn client(&self) -> Client;
+    async fn db(&self, name: &str) -> Database;
+    async fn find_all<T: TypedCouchDocument>(&self, name: &str) -> Vec<T>;
+    async fn find<T: TypedCouchDocument>(&self, name: &str) -> Vec<T>;
+    async fn get<T: TypedCouchDocument>(&self, name: &str, id: &str) -> T;
+    // async fn create<T: TypedCouchDocument>(&self, name: &str, mut data: T) -> String;
+    // async fn delete<T: TypedCouchDocument>(&self, name: &str, id: &str);
+}
+
+#[async_trait]
+impl CouchTrait for CouchRepository {
+    fn client(&self) -> Client {
         couch_rs::Client::new(&self.host, &self.username, &self.password).unwrap()
     }
 
-    pub async fn db(&self, name: &str) -> Database {
+    async fn db(&self, name: &str) -> Database {
         self.client().db(name).await.unwrap()
     }
 
-    pub async fn find_all<T: TypedCouchDocument>(&self, name: &str) -> Vec<T> {
+    async fn find_all<T: TypedCouchDocument>(&self, name: &str) -> Vec<T> {
         let db = self.db(name).await;
         let query = FindQuery::find_all();
         let docs = db.find::<T>(&query).await.unwrap();
         docs.rows
     }
 
-    pub async fn find<T: TypedCouchDocument>(&self, name: &str) -> Vec<T> {
+    async fn find<T: TypedCouchDocument>(&self, name: &str) -> Vec<T> {
         todo!()
         // use couch_rs::types::find::FindQuery
     }
 
-    pub async fn get<T: TypedCouchDocument>(&self, name: &str, id: &str) -> T {
+    async fn get<T: TypedCouchDocument>(&self, name: &str, id: &str) -> T {
         let db = self.db(name).await;
         db.get::<T>(id).await.unwrap()
     }
 
-    pub async fn create<T: TypedCouchDocument>(&self, name: &str, mut data: T) -> String {
+    // インタフェースが悪そう
+    async fn create<T: TypedCouchDocument>(&self, name: &str, mut data: T) -> String {
         let db = self.db(name).await;
         let details = db.create(&mut data).await;
         details.unwrap().id
     }
 
-    pub async fn delete<T: TypedCouchDocument>(&self, name: &str, id: &str) {
+    async fn delete<T: TypedCouchDocument>(&self, name: &str, id: &str) {
         let db = self.db(name).await;
         if let Some(doc) = db.get::<T>(id).await.ok() {
             db.remove(&doc).await;
