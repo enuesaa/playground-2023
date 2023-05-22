32c3018761a803939a74e480ed9c98d925613678
enuesaa <nsrciog@gmail.com>
2023-05-23 02:15:34 +0900

    feat(api): api のレスポンスをテストするコードのサンプルを用意した

--- packages/aero-api/src/router.rs
+++ packages/aero-api/src/router.rs
@@ -25,9 +25,37 @@ pub fn app() -> Router {
         .route("/apps/:app_id", get(get_app))
         .route("/apps", post(create_app))
         .route("/apps/:app_id", delete(delete_app))
-        .route("/apps/:app_id/versions", get(list_app_versions))
-        // update meta 
-        .route("/apps/:app_id/versions", post(create_appversion))
-        .route("/apps/:app_id/versions/:version_id/invoke", post(invoke_app_version))
+        // .route("/apps/:app_id/versions", get(list_app_versions))
+        // // update meta 
+        // .route("/apps/:app_id/versions", post(create_appversion))
+        // .route("/apps/:app_id/versions/:version_id/invoke", post(invoke_app_version))
         .with_state(appcase)
+}
+
+
+// see https://github.com/tokio-rs/axum/blob/main/examples/testing/src/main.rs
+#[cfg(test)]
+mod tests {
+    use axum::body::Body;
+    use axum::http::{Request, StatusCode};
+    use tower::ServiceExt;
+    use crate::router::app;
+    use crate::usecase::appcase::Appcase;
+
+    #[tokio::test]
+    async fn list_apps_test() {
+        // let appcase = Appcase::new(
+        //     CouchRepository::new(),
+        //     RunwasmRepository::new(),
+        // );
+
+        let app = app();
+
+        let response = app
+            .oneshot(Request::builder().uri("/").body(Body::empty()).unwrap())
+            .await
+            .unwrap();
+
+        assert_eq!(response.status(), StatusCode::OK);
+    }
 }
