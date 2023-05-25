925ca19264d6e0d7cbecb3269dd05e401c7ab0a1
enuesaa <nsrciog@gmail.com>
2023-05-25 21:58:04 +0900

    chore(code): add memo

--- packages/code-go/samples/list-files.go
+++ packages/code-go/samples/list-files.go
@@ -16,3 +16,8 @@ func ListFiles() {
 
 // see https://dskrzypiec.dev/mock-go-filesystem/
 // os.ReadDir 自体をmockしている
+// 問題はどのレイヤーでmockするか
+// - wasmで擬似的にfilesystemを再現する
+// - golangでどうにかする
+// そもそも os.ReadDir を再現したところで、そのくらい価値があるのか。
+// ui を再考する必要がある
