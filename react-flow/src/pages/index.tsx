d62218e44ef0974da8a16377525c91539ef20e4a
enuesaa <nsrciog@gmail.com>
2023-05-29 00:12:30 +0900

    feat: try react-flow

--- react-flow/src/pages/index.tsx
+++ react-flow/src/pages/index.tsx
@@ -1,7 +1,6 @@
-import React from 'react';
-import ReactFlow from 'reactflow';
-
-import 'reactflow/dist/style.css';
+import { useCallback } from 'react'
+import ReactFlow, { MiniMap, Controls, Background, useNodesState, useEdgesState, addEdge} from 'reactflow'
+import 'reactflow/dist/style.css'
 
 const initialNodes = [
   { id: '1', position: { x: 0, y: 0 }, data: { label: '1' } },
@@ -9,11 +8,27 @@ const initialNodes = [
 ];
 const initialEdges = [{ id: 'e1-2', source: '1', target: '2' }];
 
-export default function App() {
+export default function Page() {
+  // あくまで線と線を繋ぐものっぽい
+
+  const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
+  const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
+
+  const onConnect = useCallback(params => setEdges((eds) => addEdge(params, eds)), [setEdges])
+
   return (
     <div style={{ width: '100vw', height: '100vh' }}>
-      <ReactFlow nodes={initialNodes} edges={initialEdges} />
+      <ReactFlow
+        nodes={nodes}
+        edges={edges}
+        onNodesChange={onNodesChange}
+        onEdgesChange={onEdgesChange}
+        onConnect={onConnect}
+      >
+        <Controls />
+        <MiniMap /> {/* plus, minus, lock */}
+        <Background variant="dots" gap={12} size={1} /> {/* 中身はsvg? */}
+      </ReactFlow>
     </div>
   );
 }
-
