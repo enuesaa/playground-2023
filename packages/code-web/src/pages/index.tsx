5a15a4f8a8cd562717eda8349d1e8a574c042f68
enuesaa <nsrciog@gmail.com>
2023-05-28 17:57:59 +0900

    refactor(code-web): wasm を呼び出すコードに type をつけた

--- packages/code-web/src/pages/index.tsx
+++ packages/code-web/src/pages/index.tsx
@@ -10,19 +10,31 @@ export default function Page() {
       </Main>
 
       <Script id='exec-wasm' src='/wasm_exec.js' onLoad={async () => {
-        const routes: Record<string, (arg: any) => any> = {}
-        globalThis.wasmTinygoRegisterFn = (name, callback) => {
-          routes[name] = callback
+        type Routes = {
+          callname: (arg: { name: string }) => string;
+          hasPrefix: (arg: { text: string; prefix: string; }) => string;
         }
-        const go = new Go()
-        const res = await WebAssembly.instantiateStreaming(fetch('/main.wasm'), go.importObject)
-        const wasm = res.instance
-        go.run(wasm)
+        const routes: Partial<Routes> = {}
+        globalThis.wasmTinygoRegisterFn = (name, callback) => {
+          routes[name as keyof Routes] = callback
+        }
 
-        const callres = routes.callname({ name: 'aa' })
-        console.log(callres)
-        const prefixres = routes.hasPrefix({ text: 'dkjbdkj', prefix: 'd' })
-        console.log(prefixres)
+        const go = new Go()
+        // see https://ludwig125.hatenablog.com/entry/2022/03/06/080625
+        go.importObject.env['syscall/js.finalizeRef'] = ()=> {};
+
+        const source = fetch('/main.wasm')
+        const wasminit = await WebAssembly.instantiateStreaming(source, go.importObject)
+        go.run(wasminit.instance)
+
+        if (routes.callname !== undefined) {
+          const callres = routes.callname({ name: 'aa' })
+          console.log(callres)
+        }
+        if (routes.hasPrefix !== undefined) {
+          const prefixres = routes.hasPrefix({ text: 'dkjbdkj', prefix: 'd' })
+          console.log(prefixres)
+        }
       }}/>
     </>
   )
