80ef19e343c6eebe3149a78970ad63e2308f1a54
enuesaa <nsrciog@gmail.com>
2023-06-03 19:57:49 +0900

    chore: poc を進めるために具体的なケースを書き出した

---
+++ packages/web/src/pages/gojsondecode.tsx
@@ -0,0 +1,30 @@
+
+export default function Page() {
+  /**
+   * go で json decode する
+   * 
+   * import (
+   *     "encoding/json"
+   * )
+   * 
+   * func main {
+   *     json.Marshal(s)
+   * }
+   * 
+   * 
+   * - usage
+   * - wasm (demo)
+   * - link to go doc
+   * - フロー図 (mermaid)
+   * 
+   * - エラー文言
+   *   - json decode に失敗したとき
+   * 
+   * - 他言語との比較
+   *   - Python
+   *   - Node.js
+   * 
+   * もっと変わったことをしたいなあ
+   */
+  return (<></>)
+}
