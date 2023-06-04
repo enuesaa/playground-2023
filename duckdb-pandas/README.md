429c32c732b57d5a062f92257d7f0bdc2afe3165
enuesaa <nsrciog@gmail.com>
2023-06-04 14:54:41 +0900

    feat(duckdb-pandas): 触ってみた

---
+++ duckdb-pandas/README.md
@@ -0,0 +1,17 @@
+# duckdb-pandas
+
+SQLiteみたいにファイルベースのDBらしい。  
+おそらくデータ分析用途。pandas から import したりできる
+
+- インタフェースはSQL
+- [duckdb/duckdb-wasm](https://github.com/duckdb/duckdb-wasm) で wasm 化できる模様
+- よくわからないが python client は duckdb 本体のリポジトリにあるのかな。
+
+## 所感
+- だいぶ面白そう
+- データ分析用途でどのくらい活用できるか。
+  基本統計量ぐらいは取れそうだが、その先、、検定とかできるのか、、できそうなイメージあるが。
+
+
+## Links
+- [duckdb/duckdb](https://github.com/duckdb/duckdb)
--- duckdb-pandas/app/main.py
+++ duckdb-pandas/app/main.py
@@ -3,5 +3,27 @@ import pandas
 
 def main():
     my_df = pandas.DataFrame.from_dict({'a': [42]})
-    con = duckdb.connect('file.db')
-    con.sql("CREATE TABLE my_table AS SELECT * FROM my_df")
+    con = duckdb.connect('data.db')
+
+    # ただの select
+    # table が標準出力される
+    con.sql("SELECT 42").show()
+
+    # table が存在するとエラーになる
+    con.sql('CREATE TABLE integers(i INTEGER)')
+    con.sql('INSERT INTO integers VALUES (42)')
+    con.sql('INSERT INTO integers VALUES (10)')
+    con.sql('INSERT INTO integers VALUES (11)')
+    con.sql('INSERT INTO integers VALUES (0)')
+    con.sql('INSERT INTO integers VALUES (900)')
+    con.sql('INSERT INTO integers VALUES (302)')
+    con.sql('INSERT INTO integers VALUES (30)')
+    con.sql('INSERT INTO integers VALUES (59)')
+    con.sql('SELECT * FROM integers').show()
+
+    # 基本統計量的なものが取れる。箱ひげ図を作れるんじゃないか
+    con.sql('SUMMARIZE SELECT * FROM integers').show()
+
+    con.sql("CREATE TABLE dataframe AS SELECT * FROM my_df")
+    aa = con.sql('SELECT * FROM dataframe')
+    print(type(aa)) # duckdb.DuckDBPyRelation
