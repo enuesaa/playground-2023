8074d17750f4500d83b1d1a286dc5d14b3894863
enuesaa <nsrciog@gmail.com>
2023-06-19 02:58:03 +0900

    feat(golang-net-html): 要素の取得方法を調査した

--- golang-net-html/main.go
+++ golang-net-html/main.go
@@ -10,26 +10,26 @@ import (
 func main() {
 
 	r := strings.NewReader(`
-<html>
-<head>
-    <title>aaa</title>
-</head>
-<body>
-    <header>
-        <h1>aaaaa</h1>
-    </header>
-    <main>
-        <div class="cl cb">bbb</div>
-    </main>
-</body>
-</html>
+<html><head></head><body><a href="/" class="text-lg w-24">cc</a></body></html>
 `)
 
 	node, _ := html.Parse(r)
-    fmt.Printf("%+v\n", node)
-    fmt.Printf("%+v\n", node.FirstChild)
-    fmt.Printf("%+v\n", node.FirstChild.Type)
-    fmt.Printf("%+v\n", html.ElementNode)
-    fmt.Printf("%+v\n", html.TextNode)
-    fmt.Printf("%+v\n", node.NextSibling)
+    fmt.Printf("%v\n", node.Type == html.DocumentNode) // true
+    fmt.Printf("%v\n", node.FirstChild.Data) // html
+    fmt.Printf("%v\n", node.FirstChild.Type == html.ElementNode) // true
+    fmt.Printf("%v\n", node.FirstChild.FirstChild.Data) // head
+
+    bodyElm := node.FirstChild.FirstChild.NextSibling // head の次の要素を取得
+    fmt.Printf("%v\n", bodyElm.Data) // body
+
+    aElm := bodyElm.FirstChild
+    fmt.Printf("%v\n", aElm.Data) // a
+    fmt.Printf("%v\n", aElm.Type == html.ElementNode) // true
+    fmt.Printf("%v\n", aElm.Attr[0].Key) // href
+    fmt.Printf("%v\n", aElm.Attr[0].Val) // /
+    fmt.Printf("%v\n", aElm.Attr[1].Key) // class
+    fmt.Printf("%v\n", aElm.Attr[1].Val) // text-lg w-24
+
+    fmt.Printf("%v\n", aElm.FirstChild.Type == html.TextNode) // true
+    fmt.Printf("%v\n", aElm.FirstChild.Data) // cc
 }
