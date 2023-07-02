8b13d4fe0b70541312cb2a23f12a7327dfa78606
enuesaa <nsrciog@gmail.com>
2023-07-02 14:57:01 +0900

    chore(rust-hcl): change resources

--- rust-hcl/src/main.rs
+++ rust-hcl/src/main.rs
@@ -2,47 +2,15 @@ use serde_json::Value;
 
 fn main() {
     let input = r#"
-    resource "something_resource" "main" {
-        name = "main-resource"
-
-        aaaaa {
-            bb = "cc"
-        }
+    resource "note" "main" {
+        name = "something main note"
     }
 
-    resource "aws_codebuild_project" "main" {
-        name          = "main-build"
-        service_role  = aws_iam_role.codebuild.arn
-      
-        artifacts {
-          type = "NO_ARTIFACTS"
-        }
-      
-        environment {
-          compute_type                = "BUILD_GENERAL1_SMALL"
-          image                       = "aws/codebuild/amazonlinux2-x86_64-standard:4.0"
-          type                        = "LINUX_CONTAINER"
-          image_pull_credentials_type = "CODEBUILD"
-        }
-      
-        logs_config {
-          cloudwatch_logs {
-            group_name  = "log-group"
-            stream_name = "log-stream"
-          }
-        }
-      
-        source {
-          type            = "GITHUB"
-          location        = "https://github.com/example/example.git"
-          git_clone_depth = 1
-      
-          git_submodules_config {
-            fetch_submodules = true
-          }
-        }
-        source_version = "main"
-      }
+    resource "note" "sub" {
+        name = "something sub note"
+
+        created = "2023-07-02T14:49+09:00"
+    }
     "#;
 
     let value: Value = hcl::from_str(input).unwrap();
