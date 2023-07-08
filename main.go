9c934c5b978ea53f44514a4dc8a29e253547bc95
enuesaa <nsrciog@gmail.com>
2023-07-08 19:50:08 +0900

    feat: 暫定的に go で wasm のbuild処理を入れる

--- main.go
+++ main.go
@@ -1,9 +1,19 @@
 package main
 
 import (
+	"fmt"
+	"os/exec"
+
 	"github.com/enuesaa/kakkofn/contents"
 )
 
 func main() {
 	contents.Base64Decode()
+
+	// this is for development.
+	res, err := exec.Command("wasm-pack", "build", "apps/toolkit", "--out-dir", "../web/src/toolkit").Output()
+	if err != nil {
+		fmt.Printf("%v", err)
+	}
+	fmt.Printf("%v", res)
 }
