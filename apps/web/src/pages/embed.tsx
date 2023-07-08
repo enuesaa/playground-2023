3c7b73784ed14ba021fefc21134666170df17f62
enuesaa <nsrciog@gmail.com>
2023-07-08 23:28:20 +0900

    feat: post message の動作を調査した

--- apps/web/src/pages/embed.tsx
+++ apps/web/src/pages/embed.tsx
@@ -1,13 +1,24 @@
+import { useRef, MouseEventHandler } from 'react'
+
 export default function Page() {
   if (typeof window !== 'undefined') {
     window.addEventListener("message", (event) => {
-      console.log(event)
+      console.log('on child', event)
+      // event.stopPropagation()
+
     }, false);
+  }
+
+  const handleClick: MouseEventHandler<HTMLButtonElement> = (e) => {
+    e.preventDefault()
+
+    window.parent.postMessage('from child')
   }
 
   return (
     <>
       a
+      <button onClick={handleClick}>aa</button>
     </>
   )
 }
--- apps/web/src/pages/frame.tsx
+++ apps/web/src/pages/frame.tsx
@@ -1,32 +1,27 @@
 import { useRef, MouseEventHandler } from 'react'
-import Iframe from 'react-iframe'
 
 export default function Page() {
   const frame = useRef<HTMLIFrameElement>(null)
 
+  if (typeof window !== 'undefined') {
+    // react-devtools-bridge のメッセージも catch してしまう
+    window.addEventListener("message", (event) => {
+      console.log('on parent', event)
+      // event.stopPropagation()
+    }, false);
+  }
+
   const handleClick: MouseEventHandler<HTMLButtonElement> = (e) => {
     e.preventDefault()
-    console.log(frame.current)
-
-    frame.current?.contentWindow?.postMessage('aaa')
+    frame.current?.contentWindow?.postMessage('aaa', 'http://localhost:3000')
   }
 
   return (
     <>
-      <Iframe
-        url="/embed"
-        width="640px"
-        height="320px"
-        onLoad={() => {
-          console.log('a')
-        }}
-      />
-
       <iframe
         src='/embed'
         ref={frame}
       />
-
       <button onClick={handleClick}>aa</button>
     </>
   )
