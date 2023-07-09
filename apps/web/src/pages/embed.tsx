94ecb68d6a6915e7b2ace7aa0ed3dfb280d6861e
enuesaa <nsrciog@gmail.com>
2023-07-09 18:19:18 +0900

    feat: @react-hook/event で書き直した

--- apps/web/src/pages/embed.tsx
+++ apps/web/src/pages/embed.tsx
@@ -1,16 +1,16 @@
 import { useRef, MouseEventHandler } from 'react'
+import useEvent from '@react-hook/event'
 
 export default function Page() {
-  if (typeof window !== 'undefined') {
-    window.addEventListener(
-      'message',
-      (event) => {
-        console.log('on child', event)
-        // event.stopPropagation()
-      },
-      false,
-    )
-  }
+  // if (typeof window !== 'undefined') {
+  //   window.addEventListener('message', (event) => {
+  //     console.log('on child', event)
+  //   })
+  // }
+
+  useEvent(globalThis.window, 'message', (event) => {
+    console.log('on child', event)
+  })
 
   const handleClick: MouseEventHandler<HTMLButtonElement> = (e) => {
     e.preventDefault()
--- apps/web/src/pages/frame.tsx
+++ apps/web/src/pages/frame.tsx
@@ -1,19 +1,19 @@
 import { useRef, MouseEventHandler } from 'react'
+import useEvent from '@react-hook/event'
 
 export default function Page() {
   const frame = useRef<HTMLIFrameElement>(null)
 
-  if (typeof window !== 'undefined') {
-    // react-devtools-bridge のメッセージも catch してしまう
-    window.addEventListener(
-      'message',
-      (event) => {
-        console.log('on parent', event)
-        // event.stopPropagation()
-      },
-      false,
-    )
-  }
+  // if (typeof window !== 'undefined') {
+  //   window.addEventListener('message', (event) => {
+  //     console.log('on parent', event)
+  //   })
+  // }
+
+  useEvent(globalThis.window, 'message', (event) => {
+    // https://github.com/facebook/react-devtools/issues/812
+    console.log('on parent', event)
+  })
 
   const handleClick: MouseEventHandler<HTMLButtonElement> = (e) => {
     e.preventDefault()
