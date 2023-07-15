d0cb93847883e02e1cc14a9510eb98f651082a71
enuesaa <nsrciog@gmail.com>
2023-07-15 23:38:49 +0900

    feat(flywaywind): cloudflare workers kv に値を保存できるようにした

--- packages/flywaywind/src/index.ts
+++ packages/flywaywind/src/index.ts
@@ -1,8 +1,24 @@
 import { Hono } from 'hono'
 
-const app = new Hono()
+// declare const MY_KV: KVNamespace;
+type Bindings = {
+  MY_KV: KVNamespace
+}
+
+const app = new Hono<{ Bindings: Bindings }>()
 
 app.get('/', (c) => c.text('Hello Hono!'))
-app.get('/api', (c) => c.text('Hello from api!'))
+app.get('/api/items/aa', async (c) => {
+  const item = await c.env.MY_KV.get('aa')
+  return c.json({success: true, aa: item})
+})
+
+app.post('/api/items', async (c) => {
+  const { value } = await c.req.json()
+  if (typeof value === "string") {
+    await c.env.MY_KV.put("aa", value)
+  }
+  return c.json({success: true})
+})
 
 export default app
