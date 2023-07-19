ff8e4c2bd961bca5a9c682a29654926d88e20ce4
enuesaa <nsrciog@gmail.com>
2023-07-20 01:42:17 +0900

    feat: move files to /web from /apps/web

--- web/index.tsx
+++ web/index.tsx
@@ -1,11 +1,11 @@
-import * as React from "react"
+import { StrictMode } from "react"
 import * as ReactDOM from "react-dom/client"
 import { BrowserRouter } from "react-router-dom"
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
+  <StrictMode>
     <BrowserRouter>
       b
     </BrowserRouter>
-  </React.StrictMode>
+  </StrictMode>
 )
