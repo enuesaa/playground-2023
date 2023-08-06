acf45b01483b3db4ecf0b9faf3d801f07db38cfd
enuesaa <nsrciog@gmail.com>
2023-08-06 16:49:52 +0900

    Revert "feat: try to use gatsby js"
    This reverts commit c924fa2af54cf3f9dadf8bc26dde501e3026c5b7.

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -1,11 +1,3 @@
-import React from 'react'
-import { Header } from '@/components/common/Header'
-
 export default function Page() {
-  return (
-    <main>
-      <Header />
-      <h1>Hello!</h1>
-    </main>
-  )
+  return <></>
 }
