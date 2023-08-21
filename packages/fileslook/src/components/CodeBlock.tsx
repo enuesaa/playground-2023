57574435c47265ad374f0986043c2d9c374dd403
enuesaa <nsrciog@gmail.com>
2023-08-22 03:33:38 +0900

    feat: change color theme

--- packages/fileslook/src/components/CodeBlock.tsx
+++ packages/fileslook/src/components/CodeBlock.tsx
@@ -1,5 +1,6 @@
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
 import { a11yDark } from 'react-syntax-highlighter/dist/cjs/styles/prism'
+import { CSSProperties } from 'react'
 
 type Props = JSX.IntrinsicElements['code']
 export const CodeBlock = ({ className, children }: Props) => {
@@ -9,8 +10,15 @@ export const CodeBlock = ({ className, children }: Props) => {
     return (<>children</>)
   }
 
+  const theme = {
+    ...a11yDark,
+  } as {[key: string]: CSSProperties}
+  theme[':not(pre) > code[class*="language-"]']['background'] = '#282a36'
+  theme['pre[class*="language-"]']['background'] = '#282a36'
+  console.log(theme)
+
   return (
-    <SyntaxHighlighter style={a11yDark} {...(language !== '' && {language})}>
+    <SyntaxHighlighter style={theme} {...(language !== '' && {language})}>
       {children}
     </SyntaxHighlighter>
   )
