6420dfbebf9406bf3e3a0bf1984b2530b6b801ac
enuesaa <nsrciog@gmail.com>
2023-08-22 00:37:43 +0900

    fix: add dev dependencies to @types/react-syntax-highlighter

--- packages/fileslook-example/src/pages/index.tsx
+++ packages/fileslook-example/src/pages/index.tsx
@@ -1,3 +1,30 @@
+import { Files, type FileInfo } from '@enuesaa/fileslook'
+
 export default function Page() {
-  return (<></>)
+  const files: FileInfo[] = [
+    {
+      path: 'index.ts',
+      language: 'typescript',
+      content: 'import a from "b" \n'
+    },
+    {
+      path: 'main.go',
+      language: 'go',
+      content: `
+      package main
+
+      import ("fmt")
+
+      func main() {
+        fmt.Println("aa")
+      }
+      `
+    }
+  ]
+
+  return (
+    <section>
+      <Files files={files} opened={'main.go'} hideSidebar={true} />
+    </section>
+  )
 }
