6d5ae059b3e8298ba90273a9ac5f3b1cbcba4d67
enuesaa <nsrciog@gmail.com>
2023-08-23 02:22:46 +0900

    feat: change theme

--- packages/fileslook/src/components/CodeBlock.tsx
+++ packages/fileslook/src/components/CodeBlock.tsx
@@ -1,6 +1,5 @@
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
-import { a11yDark } from 'react-syntax-highlighter/dist/cjs/styles/prism'
-import { CSSProperties } from 'react'
+import { oneDark } from 'react-syntax-highlighter/dist/cjs/styles/prism'
 
 type Props = JSX.IntrinsicElements['code']
 export const CodeBlock = ({ className, children }: Props) => {
@@ -10,15 +9,8 @@ export const CodeBlock = ({ className, children }: Props) => {
     return (<>children</>)
   }
 
-  const theme = {
-    ...a11yDark,
-  } as {[key: string]: CSSProperties}
-  theme[':not(pre) > code[class*="language-"]']['background'] = '#282a36'
-  theme['pre[class*="language-"]']['background'] = '#282a36'
-  console.log(theme)
-
   return (
-    <SyntaxHighlighter style={theme} {...(language !== '' && {language})}>
+    <SyntaxHighlighter style={oneDark} {...(language !== '' && {language})}>
       {children}
     </SyntaxHighlighter>
   )
--- packages/fileslook/src/components/FileContent.tsx
+++ packages/fileslook/src/components/FileContent.tsx
@@ -1,13 +1,13 @@
 import { type FileInfo } from '../type'
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
-import { a11yDark } from 'react-syntax-highlighter/dist/cjs/styles/prism'
+import { oneDark } from 'react-syntax-highlighter/dist/cjs/styles/prism'
 
 type Props = {
   file: FileInfo|null;
 }
 export const FileContent = ({ file }: Props) => {
   return (
-    <SyntaxHighlighter style={a11yDark} language={file?.language}>
+    <SyntaxHighlighter style={oneDark} language={file?.language}>
       {file?.content ?? ''}
     </SyntaxHighlighter>
   )
