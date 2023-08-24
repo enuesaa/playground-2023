d75f96ac9bb6c11b7c469675655ff22494ab6ace
enuesaa <nsrciog@gmail.com>
2023-08-25 01:33:28 +0900

    feat(cloudscape): try link

--- cloudscape/src/pages/index.tsx
+++ cloudscape/src/pages/index.tsx
@@ -1,6 +1,7 @@
 import { Header } from '@/components/common/Header'
 import { Main } from '@/components/common/Main'
 import Button from '@cloudscape-design/components/button'
+import Link from "@cloudscape-design/components/link";
 
 export default function TopPage() {
   return (
@@ -8,6 +9,7 @@ export default function TopPage() {
       <Header />
       <Main>
         <Button variant='primary'>button</Button>
+        <Link href="/cards">Secondary link</Link>;
       </Main>
     </>
   )
