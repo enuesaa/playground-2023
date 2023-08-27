7805a7e96642838c7c2fd6642731aac60e0c4907
enuesaa <nsrciog@gmail.com>
2023-08-27 16:46:07 +0900

    feat: try to run wasi on browser. but not working for now.

--- apps/web/src/pages/index.tsx
+++ apps/web/src/pages/index.tsx
@@ -1,6 +1,21 @@
+import { useEffect } from 'react'
+
 export default function Page() {
-  return (
-    <>
-    </>
-  )
+
+  useEffect(() => {
+    // fetch('protowasi.wasm')
+    //   .then((response) => response.arrayBuffer())
+    //   .then((bytes) => WebAssembly.instantiate(bytes, {}))
+    //   .then((results) => {
+    //     console.log(results)
+    //   });
+
+    WebAssembly.instantiateStreaming(fetch('protowasi.wasm'), {}).then(
+      (results) => {
+        console.log(results)
+      },
+    );
+  }, [])
+
+  return (<></>)
 }
