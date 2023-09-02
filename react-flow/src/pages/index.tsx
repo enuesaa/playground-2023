0e2c7b3047e82d7041ad3503aa63e5336a24ddc2
enuesaa <nsrciog@gmail.com>
2023-09-03 01:46:02 +0900

    feat: try react flow again

--- react-flow/src/pages/index.tsx
+++ react-flow/src/pages/index.tsx
@@ -1,33 +1,74 @@
 import { useCallback } from 'react'
-import ReactFlow, { MiniMap, Controls, Background, useNodesState, useEdgesState, addEdge} from 'reactflow'
+import ReactFlow, { MiniMap, Controls, Background, useNodesState, useEdgesState, addEdge, OnConnect, Panel} from 'reactflow'
 import 'reactflow/dist/style.css'
 
+// ノード
 const initialNodes = [
-  { id: '1', position: { x: 0, y: 0 }, data: { label: '1' } },
-  { id: '2', position: { x: 0, y: 100 }, data: { label: '2' } },
+  {
+    id: '1',
+    position: { x: 100, y: 100 },
+    data: { label: '1' },
+    style: { width: 100, height: 100 },
+  },
+  {
+    id: '2',
+    position: { x: 200, y: 200 },
+    data: { label: '2' },
+    style: { width: 500, height: 500 },
+  },
+  {
+    id: '3',
+    data: { label: 'child node 3' },
+    position: { x: 10, y: 10 },
+    style: { width: 100, height: 50 },
+    parentNode: '2',
+  },
+  {
+    id: '4',
+    data: { label: 'child node 3' },
+    position: { x: 110, y: 10 },
+    style: { width: 100, height: 50 },
+    parentNode: '2',
+  },
+  {
+    id: '5',
+    data: { label: 'child node 3' },
+    position: { x: 10, y: 60 },
+    style: { width: 100, height: 500 },
+    parentNode: '2',
+  },
 ];
-const initialEdges = [{ id: 'e1-2', source: '1', target: '2' }];
+// つなぐもの
+// see https://reactflow.dev/docs/examples/edges/custom-connectionline/
+const initialEdges = [{ id: '1to2', source: '1', target: '2' }];
 
 export default function Page() {
-  // あくまで線と線を繋ぐものっぽい
-
   const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
   const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
 
-  const onConnect = useCallback(params => setEdges((eds) => addEdge(params, eds)), [setEdges])
+  const onConnect: OnConnect = useCallback((params) => {
+    return setEdges(eds => addEdge(params, eds))
+  }, [setEdges])
 
   return (
     <div style={{ width: '100vw', height: '100vh' }}>
+      <button onClick={(e) => {
+        e.preventDefault();
+        initialNodes.push({id: '6', position: {x:200, y:300},data:{label:'aaa'}})
+        setNodes(initialNodes)
+      }}>a</button>
       <ReactFlow
         nodes={nodes}
         edges={edges}
         onNodesChange={onNodesChange}
         onEdgesChange={onEdgesChange}
         onConnect={onConnect}
+        fitView={true}
       >
         <Controls />
-        <MiniMap /> {/* plus, minus, lock */}
-        <Background variant="dots" gap={12} size={1} /> {/* 中身はsvg? */}
+        <MiniMap />
+        <Background gap={12} size={1} />
+        <Panel position='top-right' style={{color: '#fafafa'}}>panel</Panel>
       </ReactFlow>
     </div>
   );
