64d9bee01b5248dc540de148a99c16e859b5d2ac
enuesaa <nsrciog@gmail.com>
2023-09-20 04:00:57 +0900

    feat: try react sketch canvas

--- excalidraw-tutorial/src/App.tsx
+++ excalidraw-tutorial/src/App.tsx
@@ -1,15 +1,23 @@
-import { Excalidraw, MainMenu } from "@excalidraw/excalidraw"
+// import { Excalidraw, MainMenu } from "@excalidraw/excalidraw"
 import { CanvasPage } from './canvas'
+import { Global } from '@emotion/react'
+import { globalStyle } from './styles/global'
 
 export default function App() {
   return (
     <>
-      <div style={{ height: "100vh" }}>
-        {/* <Excalidraw>
-          <MainMenu />
-        </Excalidraw> */}
-        <CanvasPage />
-      </div>
+      <Global styles={globalStyle} />
+      <section style={{ display: 'flex', height: '100vh' }}>
+        <div style={{flex: '0 0 300px', height: '100%'}}>
+          <div style={{ width: '100%' }}>aa</div>
+        </div>
+        <div style={{flex: '1 1 auto'}}>
+          {/* <Excalidraw>
+            <MainMenu />
+          </Excalidraw> */}
+          <CanvasPage />
+        </div>
+      </section>
     </>
   )
 }
--- excalidraw-tutorial/src/canvas.tsx
+++ excalidraw-tutorial/src/canvas.tsx
@@ -3,13 +3,13 @@ import { ReactSketchCanvas } from "react-sketch-canvas";
 export const CanvasPage = () => {
 
   return (
-    <>
+    <div style={{ width: '70vw', height: '100vh', overflowX: 'auto' }}>
       <ReactSketchCanvas
-        width="600"
-        height="400"
+        width="1500px"
+        height="700px"
         strokeWidth={4}
         strokeColor="black"
       />
-    </>
+    </div>
   )
 }
