bf385173fc1bcb2179762d915fa45d64f0b05d52
nua <enuesaa@gmail.com>
2023-12-17 23:20:45 +0900

    chore: remove unused files.

--- src/lib/toolkit.ts
+++
@@ -1,8 +0,0 @@
-import { atom } from 'jotai'
-
-type Fncore = typeof import('@/toolkit/toolkit.js')
-
-export const fncoreAtom = atom<Fncore | null>(null)
-fncoreAtom.onMount = (set) => {
-  ;(async () => set(await import('@/toolkit/tookit.js')))()
-}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,23 +1,3 @@
-import { useEffect } from 'react'
-import * as wasm from '../lib/wasm/toolkit.js'
-
 export default function Page() {
-
-  useEffect(() => {
-    // fetch('protowasi.wasm')
-    //   .then((response) => response.arrayBuffer())
-    //   .then((bytes) => WebAssembly.instantiate(bytes, {}))
-    //   .then((results) => {
-    //     console.log(results)
-    //   });
-
-    // WebAssembly.instantiateStreaming(fetch('toolkit_bg.wasm'), {}).then(
-    //   (results) => {
-    //     console.log(results)
-    //   },
-    // );
-    console.log(wasm.main('a'))
-  }, [])
-
   return (<></>)
 }
