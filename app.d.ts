88f9d00e415d3f13cf6335efad56716a4c717e60
nua <enuesaa@gmail.com>
2023-12-17 23:23:14 +0900

    chore: update gitignore

--- app.d.ts
+++
@@ -1,9 +0,0 @@
-declare global {
-  var wasmTinygoRegisterFn: (name: string, callback: (arg: any) => any) => void;
-  var goWasmRoutes: {
-    decodeJson: (arg: { text: string }) => string;
-  }
-}
-
-// see https://zenn.dev/qnighy/articles/9c4ce0f1b68350
-export {} 
