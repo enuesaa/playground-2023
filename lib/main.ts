78d85f48d8e3d51875e9e755133eee2feed02f67
nua <enuesaa@gmail.com>
2023-12-17 23:33:02 +0900

    feat(lib): add sample program

--- lib/main.ts
+++ lib/main.ts
@@ -1,1 +1,11 @@
-export const a = 'a'
+export const replace = (text: string, prev: string, next: string): string => {
+  return text.replace(prev, next)
+}
+
+export const count = (text: string): number => {
+  return text.length
+}
+
+// export const json2yaml = (text: string): string => {
+// }
+// jsonformat
